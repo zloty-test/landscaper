97139ad943c1777ccf52f22d90e64765ff8bccb3
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-10-02 08:41:01 +0200

    added menu scrolling functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-
-var menu = (function(){
+var menu = (function () {
     var data = {
-        menuBtn : document.querySelector('#js-menu-btn'),
-        menuContent : document.querySelector('#js-menu-content')
+        menuBtn: document.querySelector('#js-menu-btn'),
+        menuContent: document.querySelector('#js-menu-content')
     };
-    data.menuBtn.addEventListener('click',function(){
+    data.menuBtn.addEventListener('click', function () {
         data.menuContent.classList.toggle('menu-content--on');
     });
 })();
+
+const aboutUsScroll = document.querySelectorAll(".about");
+
+aboutUsScroll.forEach((menu) => {
+    menu.addEventListener("click", () => {
+        window.scrollTo({
+            behavior: "smooth",
+            top: document.querySelector("#about").offsetTop
+        });
+    })
+});
